34393e3d650de16052510c9241300381b402f932
Koncord <koncord@tes3mp.com>
2020-02-21 17:36:19 +0800

    whoops

--- CI/before_deploy.sh
+++ CI/before_deploy.sh
@@ -4,20 +4,11 @@ cd ./build
 
 # Windows specific options
 if [ "${TRAVIS_OS_NAME}" = "windows" ]; then
-  ARCH_FILE+='win'
   choco install hub
 elif [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-  ARCH_FILE+='linux'
   sudo snap install hub --classic
 else
-  ARCH_FILE+='macos'
   brew install hub
-fi
-
-if [ $"BUILD_ARCH" = "x64" ]; then
-  ARCH_FILE+='64'
-else
-  ARCH_FILE+='32'
 fi
 
 chmod +x ../CI/deploy.sh
--- CI/deploy.sh
+++ CI/deploy.sh
@@ -9,18 +9,17 @@ ARCH_FILE='testTravisCI-1.2.3-'
 # Windows specific options
 if [ "${TRAVIS_OS_NAME}" = "windows" ]; then
   ARCH_FILE+='win'
+  if [ "${BUILD_ARCH}" = "x64" ]; then
+    ARCH_FILE+='64'
+  else
+    ARCH_FILE+='32'
+  fi
 elif [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-  ARCH_FILE+='linux'
+  ARCH_FILE+='Linux'
 else
   ARCH_FILE+='macos'
 fi
 
-if [ $"BUILD_ARCH" = "x64" ]; then
-  ARCH_FILE+='64'
-else
-  ARCH_FILE+='32'
-fi
-
-ARCH_FILE+='7.z'
+ARCH_FILE+='.7z'
 
 hub release create -da "./$ARCH_FILE" -m "$TRAVIS_TAG" $TRAVIS_TAG
